cf4114d002391f08c4bc607c71319d710c199137
igor <igor.danilov1824@gmail.com>
2024-06-26 10:43:55 +0300

    split all code on layer, add refactoring, add some fields for User Schema

--- src/utils/prepareUserData.ts
+++ src/utils/prepareUserData.ts
@@ -3,6 +3,8 @@ import { CustomUserType } from "../types/user";
 
 export const prepareUserData = (userData: User): CustomUserType => {
     return {
+        secret_code: userData.secret_code,
+        username: userData.username,
         dateOfBirth: userData.dateOfBirth,
         id: userData.id,
         email: userData.email,
@@ -12,6 +14,6 @@ export const prepareUserData = (userData: User): CustomUserType => {
         lastname: userData.lastname,
         referral: userData.referral,
         country: userData.country,
-        phone_number: userData.phone_number,
+        phone_number: userData.phone_number
     }
 }
